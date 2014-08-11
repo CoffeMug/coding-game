f316c1c10ef567915ea95582eadfd302cee4b4a2
Amin Khorsandiaghai <eaminkh@esekiws5981.rnd.ki.sw.ericsson.se>
2014-08-11 16:24:29 +0200

    Some code refactoring

--- weekly_coding_game_w33/refactor_me.erl
+++ weekly_coding_game_w33/refactor_me.erl
@@ -2,16 +2,17 @@
 -export([register_user/6,
          get_user_data/3]).
 
-%% ----------------------------------------------------------------------------------------------
+%% --------------------------------------------------------------------------------
 %% Handles User Registration 
 %%
-%% This is the only module which touches the DataBase and should always be the ONLY module which
-%% does so. 
-%% A user tries to register and we check that the WorldRegion is OKAY, and that the username is
-%% valid, and that the password is long enough and that someone with the same email does not 
-%% already exist in the database.
+%% This is the only module which touches the DataBase and should always be the 
+%% ONLY module which does so. 
+%% A user tries to register and we check that the WorldRegion is OKAY, and that 
+%% the username is valid, and that the password is long enough and that 
+%% someone with the same email does not already exist in the database.
 %%
-%% register_user(DataBase :: [{FirstName, ChosenPassword, EmailAddress, UserName, WorldRegion}],
+%% register_user(DataBase :: [{FirstName, ChosenPassword, EmailAddress, UserName, 
+%%                             WorldRegion}],
 %%               FirstName :: The first name of the person registering
 %%               ChosenPassword :: The chosen password of the person registering
 %%               EmailAddress :: The email address of the person registering
@@ -26,7 +27,7 @@
 %% Result :: ok | {error,bad_region} | {error, bad_password} | {error,bad_username} |
 %%           these errors added on 2013-07-04!
 %%          
-%% ----------------------------------------------------------------------------------------------
+%% --------------------------------------------------------------------------------
 
 register_user(_, WorldRegion) when WorldRegion < 1, WorldRegion > 6 -> 
     {error,bad_region}.
@@ -35,12 +36,31 @@ register_user(DataBase, FirstName, ChosenPassword, EmailAddress, UserName, World
     check_params(WorldRegion, FirstName, ChosenPassword, UserName, EmailAddress),
     put(db,DataBase).
 
+get_user_data(DataBase,EmailAddress,UserName) ->
+
+    L = [ Entry || Entry <- DataBase,
+                   UserName == element(4,Entry) andalso 
+                       EmailAddress == element(3,Entry) ],
+    case L of
+        [Entry] ->
+            [Entry];
+        _ ->
+            []
+    end.
+
+%% --------------------------------------------------------------------------------
+%% Helper functions
+%%
+%% --------------------------------------------------------------------------------
+
 check_params(_, FirstName, _, _, _, _) when length(FirstName) == 0 ->
     {error, bad_name};
-check_params(_, _, ChosenPassword, _, _, _) when length(ChosenPassword) < 6, length(ChosenPassword) > 20 ->
+check_params(_, _, ChosenPassword, _, _, _) when length(ChosenPassword) < 6, 
+                                                 length(ChosenPassword) > 20 ->
     {error, bad_password_length};
 check_params(_, _, ChosenPassword, _, _, _) ->
-    NonAlphaNums = [ X || X <- ChosenPassword, lists:member(X,"!@#$%^&*()_-\\|\"\'?/.,<>:;[]{}<>~`")],
+    NonAlphaNums = [ X || X <- ChosenPassword, 
+                     lists:member(X,"!@#$%^&*()_-\\|\"\'?/.,<>:;[]{}<>~`")],
     case NonAlphaNums of
         [] ->
             check_pass(ChosenPassword);
@@ -48,7 +68,8 @@ check_params(_, _, ChosenPassword, _, _, _) ->
             {error,bad_password}
     end.
 check_pass(ChosenPassword) ->
-    case [ D || D <- [$0,$1,$2,$3,$4,$5,$6,$7,$8,$9], lists:member(D, ChosenPassword) ] of
+    case [ D || D <- [$0,$1,$2,$3,$4,$5,$6,$7,$8,$9], 
+           lists:member(D, ChosenPassword) ] of
         [] ->
             {error, bad_password};
         _ ->
@@ -62,7 +83,8 @@ check_pass(ChosenPassword) ->
     end.
 
 username_not_in_passwd(ChosenPassword) ->
-    case all(fun (X) -> lists:member(X,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ " end, 
+    case all(fun (X) -> 
+             lists:member(X,"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ " end, 
              ChosenPassword) of
         true ->
                %% FIXME 
@@ -81,9 +103,6 @@ register_user4([C|R], WorldRegion, UserName, Email_address) ->
         false ->
             register_user4(R,  WorldRegion, UserName, Email_address)
     end.
-
-
-
 
 check_username_not_in_pass()
                             %% check that the password does not contain usernames
@@ -142,19 +161,6 @@ username_not_in_passwd([C|R], _) ->
             username_not_in_passwd(R, _);
         false ->
             {error,bad_name}
-    end.
-
-
-get_user_data(DataBase,EmailAddress,UserName) ->
-
-    L = [ Entry || Entry <- DataBase,
-                   UserName == element(4,Entry) andalso 
-                       EmailAddress == element(3,Entry) ],
-    case L of
-        [Entry] ->
-            [Entry];
-        _ ->
-            []
     end.
 
 already_taken_in_region(KeyUserName, KeyEmailAddress) ->
